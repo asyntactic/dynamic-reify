8ad375d246f8ca06c08e5ba1878543f023262b38
Alan Caulkins <lambda@thorne.maxint.net>
2013-02-10 15:48:28 -0600

    more unit tests - not currently passing

--- test/dynamic_reify/core_test.clj
+++ test/dynamic_reify/core_test.clj
@@ -23,6 +23,14 @@
                         bar
                         ((f3 [this]) identity))))))
          
+(deftest make-protos-conditional-ommissions
+  (is (= [['foo ['f1 ['this]] identity]]
+         (make-protos '(foo
+                        ((f1 [this]) identity)
+                        bar
+                        ((f2 [this]) (if (= 0 1)
+                                       (fn [x] 2))))))))
+
 
 (defprotocol foo (f1 [this]))
 (defprotocol bar (f2 [this]))
@@ -34,5 +42,43 @@
                                  bar
                                  ((f2 [this]) (fn [x] 2))))]
            [(.f1 i) (.f2 i)]))))
+
+
+(deftest omitted-protocol
+  (let [i (dynamic-reify (foo
+                          ((f1 [this]) (fn [x] 1))
+                          bar
+                          ((f2 [this]) (if (= 0 1)
+                                         (fn [x] 2)))))]
+    (is (= 1 (.f1 i)))
+    (is (thrown? IllegalArgumentException
+                 (.f2 i)))))
+
+
+(let [x :closed-value]
+  (defn a-closure-var [this]
+    x))
+
+(deftest defned-closure
+  (is (= :closed-value
+         (let [i (dynamic-reify (foo
+                                 ((f1 [this]) a-clojure-var)))]
+           (.f1 i)))))
+
+(deftest lexical-closure
+  (is (= :closed-value
+         (let [x :closed-value
+               i (dynamic-reify (foo
+                                 ((f1 [this]) (fn [t] x))))]
+           (.f1 i)))))
+
+(deftest conditional-reification
+  (is (= 5
+         (let [n 2
+               i (dynamic-reify (foo
+                                 ((f1 [this that]) (if (> n 3)
+                                                     (fn [t x] :greater)
+                                                     (fn [t x] x)))))]
+           (.f1 i)))))
 
 
